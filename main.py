cde0eed957025ebab32781bbc31cca01f0c9f93e
Griver <Ramzan.Elikhanov@gmail.com>
2022-02-07 17:57:59 +0300

    added function move

--- main.py
+++ main.py
@@ -11,6 +11,15 @@ params = {'ll': '37.53088,55.7031187',
 map_file = 'temp_map.png'
 
 
+def move(lon_dir=0, lat_dir=0):
+    lon = float(params['ll'].split(',')[0])
+    lat = float(params['ll'].split(',')[1])
+    spn = float(params['spn'].split(',')[0])
+    lon += spn * 2 * lon_dir
+    lat += spn * 2 * lat_dir
+    params['ll'] = f'{lon},{lat}'
+
+
 def update_image():
     response = requests.get(url, params=params)
     if not response:
@@ -36,6 +45,14 @@ pygame.display.flip()
 while running:
     event = pygame.event.wait()
     if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_RIGHT:
+            move(lon_dir=1)
+        if event.key == pygame.K_LEFT:
+            move(lon_dir=-1)
+        if event.key == pygame.K_UP:
+            move(lat_dir=1)
+        if event.key == pygame.K_DOWN:
+            move(lat_dir=-1)
         should_update = True
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
